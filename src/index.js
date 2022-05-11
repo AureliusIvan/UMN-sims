6549d109fb34581945d240252153cbd502440ec3
AureliusIvan <aurelius.ivan@student.umn.ac.id>
2022-05-11 23:10:05 +0700

    test

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import { ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
-import { createRoot } from "react-dom/client";
+import ReactDOM from 'react-dom';
 import App from './App';
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
+ReactDOM.render(
   <StrictMode>
     <ColorModeScript />
     <App />
   </StrictMode>,
+  document.getElementById('root')
 );
